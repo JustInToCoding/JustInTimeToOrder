7e88a2d2eea9f8fc51239ed559a75d89dc76d22e
Justin Steenhuis <justinsteenhuis@gmail.com>
2017-09-16 04:10:49 +0200

    Add Redux and .editorconfig

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import MainContainer from './components/MainContainer';
+
+const reducer = (state, action) => state; // For now untill reducers are made
+
+function configureStore(initialState) {
+  return createStore(reducer, , initialState);
+}
+
+const store = configureStore({});
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+			<Provider store={store}>
+        <MainContainer />
+			</Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
